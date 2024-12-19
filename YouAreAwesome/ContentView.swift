829b46c29cd7980162c640e651cf0e796ffb3c9c
Matthew DiNovo <matt.dinovo@icloud.com>
2024-12-19 15:01:13 -0600

    FIrst Upload to GutHub

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,17 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var messageString = "If the Genius bar needs help, they call you."
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.red)
+                .padding()
+                .frame(width: 300, height: 150)
+                .border(.orange, width: 2)
+           
+            HStack {
+                
+                Button("Awesome") {
+                    //TODO: This is the action that is perfromed when the button is pressed.
+                    messageString = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Button("Great") {
+                    messageString = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
+        
+            }
         }
-        .padding()
+        
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+// If the Genius bar needs help, they call you.
